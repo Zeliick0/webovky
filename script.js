1da8371eda8d14fd1b5b6fc7df7ff8fc64a97b45
Zeliick0 <zelii6969@gmail.com>
2024-05-22 20:53:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let tries = 0;
+let matches = 0;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flipped');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.flower === secondCard.dataset.flower;
+    isMatch ? disableCards() : unflipCards();
+    document.getElementById('tries').textContent = ++tries;
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    matches++;
+    if (matches === 3) {
+        showPopup();
+    }
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flipped');
+        secondCard.classList.remove('flipped');
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 6);
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+function resetGame() {
+    tries = 0;
+    matches = 0;
+    document.getElementById('tries').textContent = tries;
+    cards.forEach(card => {
+        card.classList.remove('flipped');
+        card.addEventListener('click', flipCard);
+    });
+    shuffle();
+}
+
+function showPopup() {
+    document.getElementById('popup').classList.add('visible');
+}
+
+function closePopup() {
+    document.getElementById('popup').classList.remove('visible');
+    resetGame();
+}
